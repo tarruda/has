c04e732ddadb2c5538b60754c2e6cd428e999a71
Hemanth.HM <hemanth.hm@gmail.com>
2015-07-23 10:07:59 +0530

    Using polyfill for bind

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 var hasOwn = Object.prototype.hasOwnProperty;
-
+Function.prototype.bind = require("function-bind");
 
 module.exports = function has(obj, property) {
   return hasOwn.call(obj, property);
