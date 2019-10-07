a4bb62a60687d64fe12d632ba75939775b3f0992
ExE Boss <3889017+ExE-Boss@users.noreply.github.com>
2019-10-07 10:00:00 +0200

    fix: `has(…)` returns `false` on `null` or `undefined`

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 'use strict';
 
 var bind = require('function-bind');
+var hasImpl = bind.call(Function.call, Object.prototype.hasOwnProperty);
 
-module.exports = bind.call(Function.call, Object.prototype.hasOwnProperty);
+module.exports = function hasOwnProperty(target) {
+  if (target === null || target === void 0) {
+    return false;
+  }
+
+  return hasImpl.apply(void 0, arguments);
+};
--- test/index.js
+++ test/index.js
@@ -6,5 +6,7 @@ var has = require('../');
 test('has', function (t) {
   t.equal(has({}, 'hasOwnProperty'), false, 'object literal does not have own property "hasOwnProperty"');
   t.equal(has(Object.prototype, 'hasOwnProperty'), true, 'Object.prototype has own property "hasOwnProperty"');
+  t.equal(has(null, 'hasOwnProperty'), false, 'null does not have own property "hasOwnProperty"');
+  t.equal(has(void 0, 'hasOwnProperty'), false, 'undefined does not have own property "hasOwnProperty"');
   t.end();
 });
