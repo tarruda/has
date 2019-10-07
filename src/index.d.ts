50e480a5fac2a706391f1760d1fc32aabe9c30f4
ExE Boss <3889017+ExE-Boss@users.noreply.github.com>
2019-10-07 10:10:00 +0200

    feat: Add TypeScript type definition

---
+++ src/index.d.ts
@@ -0,0 +1,7 @@
+/**
+ * Determines whether an object has a property with the specified name.
+ * @param object The object to check.
+ * @param property A property name.
+ */
+declare function hasOwnProperty(object: any, property: PropertyKey): boolean;
+export = hasOwnProperty;
